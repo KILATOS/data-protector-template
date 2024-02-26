ab5e69b40d560821d883650a95c30d0cf8ef1dc9
Konstantin Kilchitskii <kilchitskij3@gmail.com>
2024-02-26 22:00:09 +0300

    adding task-manager-api

---
+++ task-manager-api/src/test/java/org/masterleonardo/taskmanagerapi/TaskManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.masterleonardo.taskmanagerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
