568616dcc42693f6ad80d88223088794a3702cc8
Konstantin Kilchitskii <kilchitskij3@gmail.com>
2024-03-01 16:41:10 +0300

    adding api-gateway

---
+++ api-gateway/src/test/java/org/masterleonardo/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package org.masterleonardo.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
