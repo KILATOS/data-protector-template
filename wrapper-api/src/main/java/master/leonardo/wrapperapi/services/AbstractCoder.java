dfd35349ec1b8f68e630aceca3429184f7f16a0d
Konstantin Kilchitskii <kilchitskij3@gmail.com>
2024-03-20 14:06:51 +0300

    adding extra classes for encrypting

--- wrapper-api/src/main/java/master/leonardo/wrapperapi/services/AbstractCoder.java
+++ wrapper-api/src/main/java/master/leonardo/wrapperapi/services/AbstractCoder.java
@@ -1,2 +1,11 @@
-package master.leonardo.wrapperapi.services;public interface AbstractCoder {
+package master.leonardo.wrapperapi.services;
+
+import master.leonardo.wrapperapi.models.EncryptedPerson;
+
+/**
+ *
+ * @param <T> Type of DTO class, which converted to Encrypted person
+ */
+public interface AbstractCoder<T> {
+    public EncryptedPerson code(T dto);
 }
--- wrapper-api/src/main/java/master/leonardo/wrapperapi/services/MessageCoder.java
+++ wrapper-api/src/main/java/master/leonardo/wrapperapi/services/MessageCoder.java
@@ -1,12 +1,63 @@
 package master.leonardo.wrapperapi.services;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import master.leonardo.wrapperapi.DTO.PersonDTO;
 import master.leonardo.wrapperapi.models.EncryptedPerson;
+import org.apache.commons.codec.digest.MessageDigestAlgorithms;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.bouncycastle.asn1.ASN1ObjectIdentifier;
+import org.bouncycastle.asn1.pkcs.PKCSObjectIdentifiers;
+import org.bouncycastle.asn1.x509.AlgorithmIdentifier;
+import org.bouncycastle.asn1.x509.DigestInfo;
+import org.slf4j.LoggerFactory;
 
-import java.util.LinkedList;
-import java.util.List;
+import javax.crypto.Cipher;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.security.*;
+import java.security.cert.CertificateException;
 
-public class MesageCoder implements AbstractCoder<>{
+public class MessageCoder implements AbstractCoder<PersonDTO>{
+    private static final Logger logger = LogManager.getLogger(MessageCoder.class);
 
 
-    public EncryptedPerson
+    @Override
+    public EncryptedPerson code(PersonDTO dto) {
+        KeyStore keyStore;
+        try {
+             keyStore= KeyStore.getInstance("PKCS12");
+            keyStore.load(new FileInputStream("sender_keystore.p12"), "changeit".toCharArray());
+            PrivateKey privateKey =
+                    (PrivateKey) keyStore.getKey("senderKeyPair", "changeit".toCharArray());
+        } catch (IOException | NoSuchAlgorithmException | CertificateException | KeyStoreException |
+                 UnrecoverableKeyException e) {
+            throw new RuntimeException(e);
+        }
+
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        String stringToEncrypt;
+
+        try {
+            stringToEncrypt = objectMapper.writeValueAsString(dto);
+        } catch (JsonProcessingException e) {
+            logger.error(e.getMessage());
+            throw new RuntimeException(e);
+        }
+
+        MessageDigest md = null;
+        try {
+            md = MessageDigest.getInstance("SHA-256");
+        } catch (NoSuchAlgorithmException e) {
+            logger.error(e.getMessage());
+            throw new RuntimeException(e);
+        }
+        byte[] messageHash = md.digest(stringToEncrypt.getBytes());
+
+
+        return null;
+    }
+
 }
